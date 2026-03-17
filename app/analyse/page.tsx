b5e5243f3a49d3c3e860690511713ddc6173cab1
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-03-17 16:41:11 +0100

    Update page.tsx

--- app/analyse/page.tsx
+++ app/analyse/page.tsx
@@ -1,1 +1,10 @@
+import { Suspense } from "react";
+import AnalyseClient from "./AnalyseClient";
 
+export default function Page() {
+  return (
+    <Suspense fallback={null}>
+      <AnalyseClient />
+    </Suspense>
+  );
+}
